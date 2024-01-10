c08a7cf57baaa6bcc92c25e5d5e21114e7ecb5f2
A3thyr <a3therys@gmail.com>
2024-01-10 19:07:35 +0300

    feat:add sorting and filtering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,38 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 export const Home = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortId, setSortId] = useState({
+    name: 'популяронсти',
+    sort: 'rating',
+  });
+  const [categoriesIndex, setCategoriesIndex] = useState(0);
 
   useEffect(() => {
-    fetch('https://659d95dd47ae28b0bd34a20f.mockapi.io/pizzas')
+    setIsLoading(true);
+
+    const sortBy = sortId.sort.replace('-', '');
+    const order = sortId.sort.includes('-') ? 'asc' : 'desc';
+    const category = categoriesIndex > 0 ? `category=${categoriesIndex}` : '';
+
+    fetch(
+      `https://659d95dd47ae28b0bd34a20f.mockapi.io/pizzas?${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((response) => response.json())
       .then((array) => {
         setItems(array);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoriesIndex, sortId]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoriesIndex}
+          onClickCategory={(index) => setCategoriesIndex(index)}
+        />
+        <Sort value={sortId} onClickSort={(index) => setSortId(index)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
